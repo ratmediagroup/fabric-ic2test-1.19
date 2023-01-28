f9b582fab0be11f158f2dd29057a11300db1d54b
ratmediagroup <18minimus18@ratmediagroup.co.uk>
2023-01-28 17:25:51 +0000

    Initial commit

---
+++ src/main/java/net/ratmediagroup/ic2test/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package net.ratmediagroup.ic2test.mixin;
+
+import net.ratmediagroup.ic2test.ic2hammer;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info)
+	{
+		ic2hammer.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
